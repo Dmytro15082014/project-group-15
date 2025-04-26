8a0907830166dc8b2eacbc3a8695decb4971adbb
VadymSnizhko <snizhko@gmail.com>
2025-04-26 12:57:30 +0300

    feat: add adapted CSS for Reviews section

---
+++ src/js/reviews.js
@@ -0,0 +1,4 @@
+const reviews = {
+    name: 'Dmytro Nazarenko',
+    text: 'I have the honor to recommend him as an exceptional professional in his field. His knowledge and expertise are undeniable. Cooperation with him always brings impressive results.'
+}
